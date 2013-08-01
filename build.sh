8c86bb2e66b211aab08d2c4cc5ccee007193ea4e
Mike Gaffney <mike@uberu.com>
2013-07-31 21:33:10 -0700

    use proxy for node files as well

--- build.sh
+++ build.sh
@@ -8,4 +8,8 @@ then
 	docker tag u1210_nodebase u1210_yo_simple
 fi
 
+HOST_IP=`ifconfig docker0 |grep inet|head -1|sed 's/\:/ /'|awk '{print $3}'`
+
+cat npmrc.template | sed "s/HOST_IP/$HOST_IP/" > npmrc
+
 docker build -t u1210_yo_simple .
